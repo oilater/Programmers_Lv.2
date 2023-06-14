a184c18721c85b3a111cfb8c3cff06f42d4a89ca
김성현 <oilater@naver.com>
2023-06-14 16:03:58 +0900

    pure recursive

--- recursive.js
+++ recursive.js
@@ -33,3 +33,28 @@
 // }
 
 // console.log(factorial(4));
+
+// 순수 재귀
+function collectOddValues(arr) {
+  let newArr = [];
+  if (arr.length == 0) return newArr;
+
+  if (arr[0] % 2 !== 0) newArr.push(arr[0]);
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  return newArr;
+}
+
+//헬퍼 메소드
+function collectOddValues(arr) {
+  let result = [];
+
+  function helper(helperInput) {
+    if (helperInput.length == 0) return;
+    if (helperInput[0] % 2 !== 0) result.push(helperInput[0]);
+    helper(helperInput.slice(1));
+  }
+  helper(arr);
+}
+
+console.log(collectOddValues([1, 2, 3, 4, 5]));
