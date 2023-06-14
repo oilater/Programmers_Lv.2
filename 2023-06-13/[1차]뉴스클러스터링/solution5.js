9311ac021fff5c1f322cdfdf5484c4b6df2d0e5f
김성현 <oilater@naver.com>
2023-06-14 15:45:22 +0900

    recursive practice

---
+++ 2023-06-13/[1차]뉴스클러스터링/solution5.js
@@ -0,0 +1,33 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17677?language=javascript
+
+function explode(text) {
+  const result = [];
+  for (let i = 0; i < text.length; i++) {
+    const node = text.substr(i, 2);
+    if (node.match(/[A-Za-z]{2}/)) result.push(node.toLowerCase());
+  }
+  return result;
+}
+
+function solution(str1, str2) {
+  const arr1 = explode(str1);
+  const arr2 = explode(str2);
+  const set = new Set([...arr1, ...arr2]);
+  let union = 0;
+  let intersection = 0;
+
+  set.forEach((item) => {
+    const has1 = arr1.filter((x) => x === item).length;
+    const has2 = arr2.filter((x) => x === item).length;
+    union += Math.max(has1, has2);
+    intersection += Math.min(has1, has2);
+  });
+  return union === 0 ? 65536 : Math.floor((intersection / union) * 65536);
+}
+
+console.log(solution("FRANCE", "french")); // 16384
+console.log(solution("handshake", "shake hands")); // 65536
+console.log(solution("aa1+aa2", "AAAA12")); // 43690
+console.log(solution("E=M*C^2", "e=m*c^2")); // 65536
+console.log(solution("1AA", "11AAAA")); // 21845
+console.log(solution("abab", "baba")); // 32768
---
+++ recursive.js
@@ -0,0 +1,35 @@
+// function countDown(num) {
+//   if (num <= 0) {
+//     console.log("All done!");
+//     return;
+//   }
+//   console.log(num);
+//   num--;
+//   countDown(num);
+// }
+
+// countDown(3);
+
+// function sumRange(num) {
+//   if (num === 1) return 1;
+//   return num + sumRange(num - 1);
+// }
+
+// console.log(sumRange(4)); // 10
+
+// function factorial(num) {
+//   if (num == 1) {
+//     return 1;
+//   }
+//   return num * factorial(num - 1);
+// }
+
+// function factorial(num) {
+//   let total = 1;
+//   for (let i = num; i > 1; i--) {
+//     total *= i;
+//   }
+//   return total;
+// }
+
+// console.log(factorial(4));
